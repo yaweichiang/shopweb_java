c6283c268f61beeab0a2d370e942474b879a5d15
yaweichiang <yawei19901212@gmail.com>
2022-06-17 15:57:11 +0800

    前端管理者日期天數查詢訂單串接後端 orderlist api

--- src/com/yawei/api/OrderListAPI.java
+++ src/com/yawei/api/OrderListAPI.java
@@ -24,11 +24,14 @@ public class OrderListAPI extends HttpServlet {
             System.out.println("=>"+str);
         }
         if(subPath[1].equals("id")){
+            //依照會員id 查詢所有訂單
             if(req.getSession().getAttribute("userid")!=null){
+                //會員查詢個人訂單
                 String id = req.getSession().getAttribute("userid").toString();
-                System.out.println("user=>"+id+","+subPath[2]);
+                System.out.println("個人查詢全部訂單user=>"+id+","+subPath[2]);
                 System.out.println(id.equals(subPath[2]));
                 if(id.equals(subPath[2])) {
+                    //確認查詢人與所查詢id為相同 則進行查詢
                     result = MySqlConnect.getMySql().getOrderListByMemberId(id);
                     System.out.println(result);
                     out.print(result.toString());
@@ -36,7 +39,8 @@ public class OrderListAPI extends HttpServlet {
                     out.print("error");
                 }
             }else if(req.getSession().getAttribute("managerid")!=null){
-                System.out.println("user=>"+subPath[1]);
+                //管理者查詢會員訂單
+                System.out.println("管理者查詢會員訂單user=>"+subPath[1]);
                 result = MySqlConnect.getMySql().getOrderListByMemberIdforManager(subPath[2]);
                 System.out.println(result);
                 out.print(result.toString());
@@ -44,16 +48,21 @@ public class OrderListAPI extends HttpServlet {
                 out.print("error");
             }
         }else if(subPath[1].equals("date")){
+            //依照日期查詢訂單
             if(req.getSession().getAttribute("managerid")!=null){
+                System.out.println("管理者依照日期查詢訂單 date=>"+subPath[2]);
                 result = MySqlConnect.getMySql().getOrderListByDate(subPath[2]);
-                out.print(result);
+                out.print(result.toString());
             }else{
                 out.print("error");
             }
         }else if(subPath[1].equals("days")){
+            //依照天數查詢訂單
             if(req.getSession().getAttribute("managerid")!=null){
+                System.out.println("管理者依照天數查詢訂單 date=>"+subPath[2]);
                 result = MySqlConnect.getMySql().getOrderListByDays(subPath[2]);
-                out.print(result);
+                System.out.println(result);
+                out.print(result.toString());
             }else{
                 out.print("error");
             }
